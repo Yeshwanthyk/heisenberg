7800a40f8871a3b4bc9059ba808ad3a02d7c5330
yeshwanthyk <yeshwanth.yk@gmail.com>
2013-10-19 08:28:54 -0700

    Add the rest of the code to parse text blob
    Need to push the data to redis

--- parse_quotes.py
+++ parse_quotes.py
@@ -3,34 +3,42 @@
 # The list of all the lines in the files
 alist = []
 
-# Temp list to hold and process the lines from
-# alist
-quote = []
-
 # The dict of all the quotes as seperated by
 # the delimiter
 output = {}
 
-# The counter for setting the key of the
-# dict
-count = 0
 
 
 def parse_quotes():
     """ This helps parse all the quotes from the txt file given based
         on a delimiter (in this case "EOL")
     """
+    # Temp list to hold and process the lines from
+    # alist
+    quote = []
+
+    # The counter for setting the key of the
+    # dict
+    count = 0
+
     f = open("BreakingBadQuotes.txt")
 
     for line in f.readlines():
         alist.append(line.strip())
 
-    return alist[0]
+    for i in alist:
+        quote.append(i)
+        if i == "EOL":
+            quote.pop()
+            output[count] = quote
+            count += 1
+            quote = []
+            pass
+
+    return output
 
 if __name__ == '__main__':
 
     answer = parse_quotes()
     print answer
-
-
 
