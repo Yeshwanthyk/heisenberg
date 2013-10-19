5246c2e3a461bcd2e9ea06ccebe958c573f51e83
yeshwanthyk <yeshwanth.yk@gmail.com>
2013-10-18 20:44:23 -0400

    Initial commit

---
+++ parse_quotes.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+""" This helps parse all the quotes from the txt file given based
+    on a delimiter (in this case "EOL")
+"""
+quote = []
+
+
+
+
+f = open("BreakingBadQuotes.txt")
+
+
