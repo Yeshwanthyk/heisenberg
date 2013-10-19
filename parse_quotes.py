bf5291d9b256cc85495c296decd331b68071aa93
yeshwanthyk <yeshwanth.yk@gmail.com>
2013-10-18 21:02:27 -0400

    Add gitignore

--- parse_quotes.py
+++ parse_quotes.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
-""" This helps parse all the quotes from the txt file given based
-    on a delimiter (in this case "EOL")
-"""
+
+# The list of all the lines in the files
+alist = []
+
+# Temp list to hold and process the lines from
+# alist
 quote = []
 
+# The dict of all the quotes as seperated by
+# the delimiter
+output = {}
+
+# The counter for setting the key of the
+# dict
+count = 0
 
 
+def parse_quotes():
+    """ This helps parse all the quotes from the txt file given based
+        on a delimiter (in this case "EOL")
+    """
+    f = open("BreakingBadQuotes.txt")
 
-f = open("BreakingBadQuotes.txt")
+    for line in f.readlines():
+        alist.append(line.strip())
+
+    return alist[0]
+
+if __name__ == '__main__':
+
+    answer = parse_quotes()
+    print answer
+
 
 
